3592b1189172ee4deb6ba38e2697700cd7565672
Anton Bondarenko <bond.anton@gmail.com>
2016-04-06 18:52:18 +0300

    Quaternions arrays demo

--- demo/02_quaternions_array.py
+++ demo/02_quaternions_array.py
@@ -9,6 +9,3 @@ quaternions_array = utils.random_quaternions_array(shape, 5)
 print(quaternions_array.shape)
 print(qf.exp(qf.log(quaternions_array)) == quaternions_array + np.eye(min(shape)))
 print(qf.log(qf.exp(quaternions_array)) == quaternions_array)
-
-random_rotation = utils.random_rotation()
-print(random_rotation)
