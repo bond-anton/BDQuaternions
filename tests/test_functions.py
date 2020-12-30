d9365e7739c4a39ea30b0fc7a54b7487988853ff
bond_anton <bond.anton@gmail.com>
2020-12-31 01:26:33 +0300

    small enhancement

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -1,18 +1,15 @@
 import numpy as np
 
-from BDQuaternions import Quaternion
+from BDQuaternions import Quaternion, UnitQuaternion
 from BDQuaternions.functions import exp, log
+from BDQuaternions.utils import random_unit_quaternion
 
 import unittest
 
 
-def rand_q(magnitude=2):
-    return Quaternion((np.random.random(4) - 0.5) * 2 * magnitude)
-
-
 def check_log_exp_q(q=None):
     if q is None:
-        q = rand_q(magnitude=2)
+        q = random_unit_quaternion()
     exp_q = exp(q)
     log_q = log(q)
     return (np.array(log(exp_q)) == np.array(q)).all() and (np.array(exp(log_q)) == np.array(q)).all()
@@ -20,7 +17,7 @@ def check_log_exp_q(q=None):
 
 def check_log_exp_number(test_num=None):
     if test_num is None:
-        magnitude = 2
+        magnitude = 1
         test_num = np.random.random(1)[0] * magnitude
     test_exp = exp(test_num)
     test_log = log(test_num)
@@ -36,18 +33,18 @@ class TestFunctions(unittest.TestCase):
         self.assertTrue(check_log_exp_q())
         q = Quaternion(np.array([0, 0, 0, 0], dtype=np.double))
         self.assertTrue(check_log_exp_q(q))
-        q = Quaternion(np.array([0, 0, 0, 1], dtype=np.double))
+        q = UnitQuaternion(np.array([0, 0, 0, 1], dtype=np.double))
         self.assertTrue(check_log_exp_q(q))
-        q = Quaternion(np.array([1, 0, 0, 0], dtype=np.double))
+        q = UnitQuaternion(np.array([1, 0, 0, 0], dtype=np.double))
         self.assertTrue(check_log_exp_q(q))
 
     def test_log_exp_number(self):
         self.assertTrue(check_log_exp_number())
 
     def test_arrays(self):
-        qs = check_log_exp_q([rand_q(magnitude=1) for _ in range(5)])
+        qs = check_log_exp_q([random_unit_quaternion() for _ in range(5)])
         self.assertTrue(qs)
-        ns = check_log_exp_number([np.random.random(1)[0] * 5 for _ in range(5)])
+        ns = check_log_exp_number([np.random.random(1)[0] for _ in range(5)])
         self.assertTrue(ns)
 
     def test_raises(self):
