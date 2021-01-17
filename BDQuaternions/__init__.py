2ea90d26c7cc44e99f918f95c06fe05bc714daa2
bond_anton <bond.anton@gmail.com>
2021-01-17 22:47:59 +0300

    Version 0.2.15. Corrected __init__.py

--- BDQuaternions/__init__.py
+++ BDQuaternions/__init__.py
@@ -4,6 +4,3 @@ from .UnitQuaternion import UnitQuaternion
 from .Rotation import Rotation
 from .EulerAnglesConventions import Conventions, Convention
 from .EulerAngles import EulerAngles
-
-
-__all__ = ['__version__', 'Quaternion', 'UnitQuaternion', 'Rotation', 'Conventions', 'Convention', 'EulerAngles']
