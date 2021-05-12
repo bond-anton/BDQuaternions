031b281390e20838802d77da5d82a1103d79652b
bond_anton <bond.anton@gmail.com>
2021-05-12 14:41:06 +0300

    v0.2.16 Cleaned setup.py

--- setup.py
+++ setup.py
@@ -139,11 +139,10 @@ setup(
         'Topic :: Scientific/Engineering :: Mathematics',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ],
 
     keywords='Quaternion 3D rotations',
